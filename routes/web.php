f6ef75cf2e401d42190883ecb493c3f946cd46a1
asrofilnadibs <asrofilnadibs28@gmail.com>
2024-12-15 18:53:14 +0700

    first commit

--- routes/web.php
+++ routes/web.php
@@ -28,4 +28,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('/message', function () {
+
+});
+
 require __DIR__.'/auth.php';
